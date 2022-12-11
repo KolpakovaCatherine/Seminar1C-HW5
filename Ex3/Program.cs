ad48ee0b9f907c4099add85928754c829971197c
Ekaterina Kolpakova <kolpakovapost@yandex.ru>
2022-12-11 14:33:56 +0300

    Ex3 resolved

---
+++ Ex3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int size = ReadInt("Введите размер массива: ");
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+WriteArray(numbers);
+double min = numbers[0];
+double max = numbers[0];
+double result = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+result = Math.Round(max - min,2);
+Console.WriteLine($"Разница между максимальным числом = {max} и минимальным числом = {min} получается {result}");
+
+void FillArrayRandomNumbers(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
